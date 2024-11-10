f730fc2263d89a0b362620522881aeb4e77b017f
damien <damien.bloch@institutoptique.fr>
2024-11-10 14:23:48 +0100

    Add docstring to caqtus_parsing.nodes

--- bindings/python/src/caqtus_parsing/nodes/__init__.py
+++ bindings/python/src/caqtus_parsing/nodes/__init__.py
@@ -1,3 +1,5 @@
-from ._nodes import Variable, Expression, Quantity, UnitTerm
+"""Contains the AST nodes for the caqtus_parsing module."""
+
+from ._nodes import Variable, Expression, Quantity, UnitTerm, Call
 
 __all__ = ["Variable", "Expression", "Quantity", "UnitTerm"]
